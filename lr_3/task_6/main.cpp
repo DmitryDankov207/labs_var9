0db230892d2f8dc967bdafca10fd50d697fd7f4e
Dmitry Dankov <12ddankov12@gmail.com>
2020-06-08 22:34:34 +0300

    corrected mistakes in 6 task

--- lr_3/task_6/main.cpp
+++ lr_3/task_6/main.cpp
@@ -6,29 +6,30 @@ string getText(string fileName) {
     fstream fin(fileName);
     string text;
     fin >> text;
-    fclose(fin);
+    fin.close();
     return text;
 }
 
 void setText(string fileName, string text) {
     fstream fout(fileName);
     fout << text;
-    fclose(fout);
+    fout.close();
 }
 
 void xorFile() {
     string fileName;
     cin >> fileName;
-    text = getText(fileName);
+    string text = getText(fileName);
     int xorVal;
     cin >> xorVal;
-    for(size_t i = 0; i < strlen(text); i++) {
-        text[i] = text[i] ^ xorval;
+    for(size_t i = 0; i < text.length(); i++) {
+        text[i] = text[i] ^ xorVal;
     }
     setText(fileName, text);
 }
 
 int main() {
+    xorFile();
     auto pr = Product("soup", 5);
     cout << pr.getName() << endl << pr.getCost();
 }
